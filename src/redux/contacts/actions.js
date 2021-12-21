07eb83138401070e60a7e89e8338a379c76598da
Alex Shepel <iam.alex.shepel@gmail.com>
2021-12-21 12:56:18 +0200

    do task with redux slice technique

--- src/redux/contacts/actions.js
+++ src/redux/contacts/actions.js
@@ -1,12 +1,3 @@
-import { createAction, nanoid } from '@reduxjs/toolkit';
+import slice from './slice';
 
-const setItems = createAction('contacts/set_items');
-const addItem = createAction('contacts/add_item', (name, number) => {
-  return {
-    payload: { name, number, id: nanoid() },
-  };
-});
-const removeItem = createAction('contacts/remove_item');
-const setFilter = createAction('contacts/set_filter');
-
-export { setItems, addItem, removeItem, setFilter };
+export const { setItems, addItem, removeItem, setFilter } = slice.actions;
--- src/redux/contacts/reducer.js
+++ src/redux/contacts/reducer.js
@@ -1,27 +1,3 @@
-import { setItems, addItem, removeItem, setFilter } from './actions';
-import { createReducer } from '@reduxjs/toolkit';
-import { combineReducers } from 'redux';
+import slice from './slice';
 
-// reducer state template
-// {
-//   items: [],
-//   filter: '',
-// }
-
-const itemsReducer = createReducer([], builder => {
-  builder
-    .addCase(setItems, (_, { payload }) => payload)
-    .addCase(addItem, (state, { payload }) => [...state, payload])
-    .addCase(removeItem, (state, { payload }) =>
-      state.filter(item => item.id !== payload),
-    );
-});
-
-const filterReducer = createReducer('', builder => {
-  builder.addCase(setFilter, (_, { payload }) => payload);
-});
-
-export default combineReducers({
-  items: itemsReducer,
-  filter: filterReducer,
-});
+export default slice.reducer;
